d383f6f6cb6b114aec1db53fb44e343e04f3c2a8
Yau King Man <yaukingman1128@gmail.com>
2022-07-19 15:35:06 +0800

    0719 加上顯示產品明細，跟購物車功能結合_P+Y

--- prjIHealth/ViewModels/CProductViewModel.cs
+++ prjIHealth/ViewModels/CProductViewModel.cs
@@ -8,7 +8,22 @@ namespace prjIHealth.ViewModels
 {
     public class CProductViewModel
     {
+        public CProductViewModel()
+        {
+            _prod = new TProduct();
+            TOrderDetails = new HashSet<TOrderDetail>();
+            TProductsImages = new HashSet<TProductsImage>();
+            TTrackLists = new HashSet<TTrackList>();
+        }
+        private TProduct _prod;
+
         IHealthContext db = new IHealthContext();
+
+        public TProduct product
+        {
+            get { return _prod; }
+            set { _prod = value; }
+        }
 
         public IEnumerable<TProduct> ProductList
         {
@@ -18,5 +33,53 @@ namespace prjIHealth.ViewModels
                 return pList;
             }
         }
+
+
+
+        public int FProductId
+        {
+            get { return _prod.FProductId; }
+            set { _prod.FProductId = value; }
+        }
+        
+        public string FProductName
+        {
+            get { return _prod.FProductName; }
+            set { _prod.FProductName = value; }
+        }
+        
+        public int FCategoryId
+        {
+            get { return _prod.FCategoryId; }
+            set { _prod.FCategoryId = value; }
+        }
+        
+        public decimal FUnitprice
+        {
+            get { return _prod.FUnitprice; }
+            set { _prod.FUnitprice = value; }
+        }
+        
+        public string FDescription 
+        {
+            get { return _prod.FDescription; }
+            set { _prod.FDescription = value; }
+        }
+        
+        public bool FVisible
+        {
+            get { return _prod.FVisible; }
+            set { _prod.FVisible = value; }
+        }
+        
+        public string FCoverImage
+        {
+            get { return _prod.FCoverImage; }
+            set { _prod.FCoverImage = value; }
+        }
+        public virtual TProductCategory FCategory { get; set; }
+        public virtual ICollection<TOrderDetail> TOrderDetails { get; set; }
+        public virtual ICollection<TProductsImage> TProductsImages { get; set; }
+        public virtual ICollection<TTrackList> TTrackLists { get; set; }
     }
 }
