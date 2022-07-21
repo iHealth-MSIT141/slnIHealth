810f876ca1ffdf8beef8f22737d20af6bd34777c
Lily319 <leftbehindgd2009@gmail.com>
2022-07-21 18:23:31 +0800

    更新教練專區功能_L

--- prjIHealth/Controllers/CoachController.cs
+++ prjIHealth/Controllers/CoachController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using prjIHealth.Models;
+using prjIHealth.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +10,11 @@ namespace prjiHealth.Controllers
 {
     public class CoachController : Controller
     {
+        private IHealthContext db;
+        public CoachController(IHealthContext context)
+        {
+            db = context;
+        }
         public IActionResult Index()
         {
             return View();
@@ -23,7 +30,94 @@ namespace prjiHealth.Controllers
         }
         public IActionResult RecruitmentList()
         {
-            return View();
+            //TODO 拿登入會員的ID
+            int theMemberId = 11;
+
+            var contacts = db.TCoachContacts.Where(c =>c.FCoach.FMemberId==theMemberId);
+            List<CContactViewModel> contactViewModels = new List<CContactViewModel>();
+            foreach(var c in contacts)
+            {
+                CContactViewModel contactViewModel = new CContactViewModel(db)
+                {
+                    FCoachContactId = c.FCoachContactId,
+                    FMemberId = c.FMemberId,
+                    FStatusNumber = c.FStatusNumber,
+                    FAvailableTimeNum = c.FAvailableTimeNum
+                };
+                contactViewModels.Add(contactViewModel);
+            }
+
+            return View(contactViewModels);
         }
+        public IActionResult changeContactStatus(TCoachContact contact)
+        {
+            db.TCoachContacts.FirstOrDefault(c => c.FCoachContactId == contact.FCoachContactId).FStatusNumber = contact.FStatusNumber;
+            db.SaveChanges();
+            return Content("");
+        }
+
+        public IActionResult showMember(int? memberId)
+        {
+            CContactViewModel member = new CContactViewModel(db)
+            {
+                FMemberId = memberId
+            };
+
+            return Json(member);
+        }
+        public IActionResult createCourse(TCourse course)
+        {
+            course.FStatusNumber = 55;
+            course.FVisible = true;
+            db.TCourses.Add(course);
+            db.SaveChanges();
+
+            return Content("");
+        }
+        public IActionResult loadContact(int? flag, int? statusNum)    //教練專區-招生紀錄:依聯繫時間排序
+        {
+            //TODO抓登入會員ID
+            int theCoachMemberId = 11;
+
+            IEnumerable<TCoachContact> contacts = null;
+            if (statusNum == 0)
+            {
+                if (flag == 1)
+                {
+                    contacts = db.TCoachContacts.Where(c => c.FCoach.FMemberId == theCoachMemberId).OrderByDescending(c => c.FContactDate);
+                }
+                else
+                {
+                    contacts = db.TCoachContacts.Where(c => c.FCoach.FMemberId == theCoachMemberId).OrderBy(c => c.FContactDate);
+                }
+            }
+            else
+            {
+                if (flag == 1)
+                {
+                    contacts = db.TCoachContacts.Where(c => c.FCoach.FMemberId == theCoachMemberId && c.FStatusNumber == statusNum).OrderByDescending(c => c.FContactDate);
+                }
+                else
+                {
+                    contacts = db.TCoachContacts.Where(c => c.FCoach.FMemberId == theCoachMemberId && c.FStatusNumber == statusNum).OrderBy(c => c.FContactDate);
+                }
+            }
+            List<CContactViewModel> contactViewModels = new List<CContactViewModel>();
+            foreach (var c in contacts)
+            {
+                CContactViewModel contactViewModel = new CContactViewModel(db)
+                {
+                    FCoachContactId = c.FCoachContactId,
+                    FMemberId = c.FMemberId,
+                    FStatusNumber = c.FStatusNumber,
+                    FAvailableTimeNum = c.FAvailableTimeNum
+                };
+                contactViewModels.Add(contactViewModel);
+            }
+            return Json(contactViewModels);
+        }
+
+
+
     }
 }
