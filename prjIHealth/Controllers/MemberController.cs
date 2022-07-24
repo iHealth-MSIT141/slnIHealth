8f945aae465d882e9003fb116830f46b79ab74ba
Robert5957 <charles.chou570114@gmail.com>
2022-07-25 01:22:11 +0800

    1.ajax重設密碼,popup登入頁面 2.0725_J

--- prjIHealth/Controllers/MemberController.cs
+++ prjIHealth/Controllers/MemberController.cs
@@ -185,10 +185,14 @@ namespace prjIHealth.Controllers
                 {
                     q.FPassword = vmodel.firstPassword;
                     _context.SaveChanges();
-                    return RedirectToAction("Login", "Member");
+                    //return RedirectToAction("Login", "Member");
+                    return Content(q.FUserName.ToString(), "text/plain", System.Text.Encoding.UTF8);
+
                 }
             }
-            return RedirectToAction("Index", "Home");
+            //return RedirectToAction("Index", "Home");
+            return Content("false", "text/plain", System.Text.Encoding.UTF8);
+
         }
 
         //========================追蹤清單===========================
